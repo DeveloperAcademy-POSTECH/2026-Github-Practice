1c032eeb217e27bb9d2d7f245144116cbed0f2e7
hannn-kim <rlagkdms030918@gmail.com>
2026-05-26 17:13:51 +0900

    한뷰 만들었음

---
+++ GithubPractice/GithubPractice/View/LearnerViews/HanView.swift
@@ -0,0 +1,22 @@
+//
+//  HanView.swift
+//  GithubPractice
+//
+//  Created by Han on 5/26/26.
+//
+
+import SwiftUI
+
+struct HanView: LearnerView {
+    var name: String = "Han"
+    
+    var team: String = "6"
+    
+    var body: some View {
+        Text("Hi!")
+    }
+}
+
+#Preview {
+    HanView()
+}
