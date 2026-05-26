f3718ae533c6b8ac6f5cc6695b412d5c09c22991
Frisson <poonkey321@gmail.com>
2026-05-26 17:13:40 +0900

    우히 xcode 하고 커밋 해볼까앙

---
+++ GithubPractice/GithubPractice/View/LearnerViews/FrissonView.swift
@@ -0,0 +1,22 @@
+//
+//  FrissonView.swift
+//  GithubPractice
+//
+//  Created by 평화 on 5/26/26.
+//
+
+import SwiftUI
+
+struct FrissonView: View {
+    var name: String = "Frisson"
+    
+    var team: String = "14"
+    
+    var body: some View {
+        Text("나는 프리송이다.")
+    }
+}
+
+#Preview {
+    FrissonView()
+}
