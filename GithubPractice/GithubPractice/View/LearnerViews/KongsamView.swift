9c584c05c499f211f45bb26b4456c30570b97bf7
Jeong Hee <konghee0411@gmail.com>
2026-05-26 17:13:50 +0900

    kongsmaView 만듦

---
+++ GithubPractice/GithubPractice/View/LearnerViews/KongsamView.swift
@@ -0,0 +1,23 @@
+//
+//  KongsamView.swift
+//  GithubPractice
+//
+//  Created by jeonghee on 5/26/26.
+//
+
+import SwiftUI
+
+struct KongsamView: LearnerView {
+    var name: String = "kongsam"
+    
+    var team: String = "14"
+    
+    var body: some View {
+        Text("Hello, world! It's Kongsam")
+        
+    }
+}
+#Preview {
+    KongsamView()
+}
+
