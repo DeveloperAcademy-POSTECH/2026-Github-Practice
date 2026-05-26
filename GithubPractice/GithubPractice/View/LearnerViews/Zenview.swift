23b46569f765dc5edb0d2524dea2d5e20f877821
kiim672 <sook8820057@gmail.com>
2026-05-26 17:13:31 +0900

    젠을 추가함

---
+++ GithubPractice/GithubPractice/View/LearnerViews/Zenview.swift
@@ -0,0 +1,22 @@
+//
+//  Zenview.swift
+//  GithubPractice
+//
+//  Created by ZEN on 5/26/26.
+//
+
+import SwiftUI
+
+struct Zenview: LearnerView {
+    let name: String = "Zen"
+    
+    let team: String = "13"
+    
+    var body: some View {
+        Text("안녕하세요 젠이에요")
+    }
+}
+
+#Preview {
+    Zenview()
+}
