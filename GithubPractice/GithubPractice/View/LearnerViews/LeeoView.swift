0a58332e58d8c7d267a2dc20a9cab174245a6567
YoungJin Jeong <81943525+luminouxx@users.noreply.github.com>
2026-05-26 17:13:41 +0900

    리이오뷰 만듬ㅋ

---
+++ GithubPractice/GithubPractice/View/LearnerViews/LeeoView.swift
@@ -0,0 +1,22 @@
+//
+//  LeeoView.swift
+//  GithubPractice
+//
+//  Created by luminoux on 5/26/26.
+//
+
+import SwiftUI
+
+struct LeeoView: LearnerView {
+    var name: String = "leeo"
+    
+    var team: String = "13"
+    
+    var body: some View {
+        Text("안녕하세요 리이오에요.")
+    }
+}
+
+#Preview {
+    LeeoView()
+}
