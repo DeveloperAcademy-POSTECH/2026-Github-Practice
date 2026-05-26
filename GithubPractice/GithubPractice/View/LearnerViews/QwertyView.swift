8d8ac4ea73354b012de668f612d26e7e091b6000
YeoGyungByun <jjjjj0604@gmail.com>
2026-05-26 17:19:12 +0900

    쿼티 뷰 생성

---
+++ GithubPractice/GithubPractice/View/LearnerViews/QwertyView.swift
@@ -0,0 +1,19 @@
+//
+//  QwertyView.swift
+//  GithubPractice
+//
+//  Created by 여경 on 5/26/26.
+//
+
+import SwiftUI
+
+struct QwertyView: LearnerView {
+    var name: String = "Qwerty"
+    var team: String = "1"
+    var body: some View {
+        Text("하이욤")
+    }
+}
+#Preview {
+    QwertyView()
+}
