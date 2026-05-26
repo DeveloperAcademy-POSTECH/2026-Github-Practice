cd768bdad09c7a7d6f636c8ad8cdcb66dc35e71d
Kim Dohyeon <elena.kim.0915@gmail.com>
2026-05-26 12:28:19 +0900

    Create ElinView.swift
    [Feat] {엘린} 뷰 제작

---
+++ GithubPractice/GithubPractice/View/LearnerViews/ElinView.swift
@@ -0,0 +1,19 @@
+//
+//  ElinView.swift
+//  GithubPractice
+//
+//  Created by 김도현 on 5/26/26.
+//
+
+import SwiftUI
+
+struct ElinView: View {
+    var body: some View {
+        Text("이름: Elin/김도현 팀: 조용한 야망")
+        
+    }
+}
+
+#Preview {
+    ElinView()
+}
