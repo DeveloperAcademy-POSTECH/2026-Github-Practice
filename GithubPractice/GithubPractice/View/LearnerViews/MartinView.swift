a63e070f050a2f38bed132f097e5341cc020f591
SongMY01 <1018myone@gmail.com>
2026-05-26 12:28:39 +0900

    [Feat] {Martin} 뷰 제작

---
+++ GithubPractice/GithubPractice/View/LearnerViews/MartinView.swift
@@ -0,0 +1,14 @@
+//
+//  MartinView.swift
+//  
+//
+//  Created by myone on 5/26/26.
+//
+
+import SwiftUI
+
+struct MartinView: View {
+    var body: some View {
+        Text("MartinView")
+    }
+}
