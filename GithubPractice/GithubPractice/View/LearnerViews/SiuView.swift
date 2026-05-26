232449b2a20549a04310ec8a99f3fa6353410441
임가은 <128891914+im-siu@users.noreply.github.com>
2026-05-26 17:13:44 +0900

    [Feat] 시우 뷰 제작

---
+++ GithubPractice/GithubPractice/View/LearnerViews/SiuView.swift
@@ -0,0 +1,22 @@
+//
+//  SiuView.swift
+//  GithubPractice
+//
+//  Created by Im gaeun on 5/26/26.
+//
+
+import SwiftUI
+
+struct SiuView: LearnerView {
+    var name: String = "Siu"
+    
+    var team: String = "11"
+    
+    var body: some View {
+        Text("나는 시우다.")
+    }
+}
+
+#Preview {
+    SiuView()
+}
