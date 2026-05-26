e5a6effdc17d0e29fb9caee3d7a99ffaa2ffe2b2
scylla <swureason@gmail.com>
2026-05-26 12:27:47 +0900

    실라 뷰 생성 커밋

---
+++ GithubPractice/GithubPractice/View/LearnerViews/ScyllaView.swift
@@ -0,0 +1,18 @@
+//
+//  ScyllaView.swift
+//  GithubPractice
+//
+//  Created by Yourim on 5/26/26.
+//
+
+import SwiftUI
+
+struct ScyllaView: View {
+    var body: some View {
+        Text("Scylla의 View")
+    }
+}
+
+#Preview {
+    ScyllaView()
+}
