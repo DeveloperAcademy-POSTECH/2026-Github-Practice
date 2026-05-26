9e088a1eae77cf5a5ab93aa428a62188c0a21cab
Sujeong(Crystal) Lee <90857836+crystaltheclear@users.noreply.github.com>
2026-05-26 17:13:33 +0900

    나도 이제 커밋마스터~

---
+++ GithubPractice/GithubPractice/View/LearnerViews/CrystalView.swift
@@ -0,0 +1,22 @@
+//
+//  CrystalView.swift
+//  GithubPractice
+//
+//  Created by 이수정 on 5/26/26.
+//
+
+import SwiftUI
+
+struct CrystalView: LearnerView {
+    var name: String = "Crystal"
+    
+    var team: String = "team 3"
+    
+    var body: some View {
+        Text("팀도네이선 파이팅!!")
+    }
+}
+
+#Preview {
+    CrystalView()
+}
