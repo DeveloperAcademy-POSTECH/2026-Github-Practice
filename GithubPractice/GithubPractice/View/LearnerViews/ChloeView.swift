d2e4368bd304229f5c46730f54b0b4186503dd7a
Saerom Kim <ksr3597@naver.com>
2026-05-26 17:13:33 +0900

    클로이뷰 만드럿음

---
+++ GithubPractice/GithubPractice/View/LearnerViews/ChloeView.swift
@@ -0,0 +1,22 @@
+//
+//  ChloeView.swift
+//  GithubPractice
+//
+//  Created by Saerom on 5/26/26.
+//
+
+import SwiftUI
+
+struct ChloeView: LearnerView {
+    var name: String = "Chloe"
+    
+    var team: String = "01"
+    
+    var body: some View {
+        Text("안냥하세요 클로이에요")
+    }
+}
+
+#Preview {
+    ChloeView()
+}
