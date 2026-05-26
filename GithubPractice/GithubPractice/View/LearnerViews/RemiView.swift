83b25584a0279982029649c0fab3ba096ae6e85d
정필규(PILLKYU CHUNG) <129770553+pillkyu@users.noreply.github.com>
2026-05-26 17:13:44 +0900

    레미뷰 만듬

---
+++ GithubPractice/GithubPractice/View/LearnerViews/RemiView.swift
@@ -0,0 +1,22 @@
+//
+//  RemiView.swift
+//  GithubPractice
+//
+//  Created by 정필규 on 5/26/26.
+//
+
+import SwiftUI
+
+struct RemiView: LearnerView {
+    let name: String = "레미"
+    
+    let team: String = "ManLearning"
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    RemiView()
+}
