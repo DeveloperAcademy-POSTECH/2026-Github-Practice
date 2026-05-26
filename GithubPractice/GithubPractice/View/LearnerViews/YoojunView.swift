2edbda9995a8e5122afb3537e71fd476e70c4b05
PARK YOOJUN <yoojun1411@gmail.com>
2026-05-26 17:13:45 +0900

    배고파...피곤해...

---
+++ GithubPractice/GithubPractice/View/LearnerViews/YoojunView.swift
@@ -0,0 +1,20 @@
+//
+//  YoojunView.swift
+//  GithubPractice
+//
+//  Created by YOOJUN PARK on 5/26/26.
+//
+
+import SwiftUI
+
+struct YoojunViewView: LearnerView {
+    
+    var name: String = "Yoojun"
+    var team: String = "6"
+    
+    var body: some View {
+        
+        Text("배고파 졸려 피곤해요...")
+        
+    }
+}
