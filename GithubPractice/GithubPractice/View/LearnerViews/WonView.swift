40e853d09c1e53b766fa660a84acc6ab87eba2e1
wondrone0113 <wondrone0113@gmail.com>
2026-05-26 12:28:05 +0900

    내이름 적기

---
+++ GithubPractice/GithubPractice/View/LearnerViews/WonView.swift
@@ -0,0 +1,18 @@
+//
+//  WonView.swift
+//  GithubPractice
+//
+//  Created by lwc0113 on 5/26/26.
+//
+
+import SwiftUI
+
+struct WonView: View {
+    var body: some View {
+        Text("나는 원이다.")
+    }
+}
+
+#Preview {
+    WonView()
+}
