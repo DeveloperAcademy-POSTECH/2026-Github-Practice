816cd0a0410d606083fb46662fc1571aa23b3002
Myong <166011549+rlagustj07@users.noreply.github.com>
2026-05-26 12:26:44 +0900

    [Feat] Myong 뷰 제작

---
+++ GithubPractice/GithubPractice/View/LearnerViews/MyongView.swift
@@ -0,0 +1,25 @@
+//
+//  MyongView.swift
+//  GithubPractice
+//
+//  Created by Myong on 5/26/26.
+//
+
+import SwiftUI
+
+struct MyongView: View {
+    var name: String = "Myong"
+    
+    var team: String = "뗔"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    MyongView()
+}
