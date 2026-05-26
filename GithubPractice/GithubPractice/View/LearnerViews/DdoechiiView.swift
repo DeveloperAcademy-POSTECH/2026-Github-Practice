452aacd8fa628b3892a2746b71803fd45543f1a2
qoqoqle <yhj526959@gmail.com>
2026-05-26 12:27:50 +0900

    또치의 첫번째 커밋

---
+++ GithubPractice/GithubPractice/View/LearnerViews/DdoechiiView.swift
@@ -0,0 +1,18 @@
+//
+//  DdoechiiView.swift
+//  GithubPractice
+//
+//  Created by kiwiisae on 5/26/26.
+//
+
+import SwiftUI
+
+struct DdoechiiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DdoechiiView()
+}
