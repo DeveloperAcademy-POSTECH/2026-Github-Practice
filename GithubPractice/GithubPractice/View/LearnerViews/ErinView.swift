600d383ed7dde213120c894523c8cd7e3b7d9bc4
Turto <121936896+ErinKYB@users.noreply.github.com>
2026-05-26 12:28:01 +0900

    [Create] ErinView.swift 시작
    역사의 시작

---
+++ GithubPractice/GithubPractice/View/LearnerViews/ErinView.swift
@@ -0,0 +1,18 @@
+//
+//  ErinView.swift
+//  GithubPractice
+//
+//  Created by Erin Yaebin Kim on 5/26/26.
+//
+
+import SwiftUI
+
+struct ErinView: View {
+    var body: some View {
+        Text("I am Erin. \nAmaze Amaze Amaze!")
+    }
+}
+
+#Preview {
+    ErinView()
+}
