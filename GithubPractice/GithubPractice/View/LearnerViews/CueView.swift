22147ab1f8791443ad4cf0dcd7030faea518b247
yuseongchae <chaeyuseong@naver.com>
2026-05-26 12:29:15 +0900

    [Feat] cue 뷰 제작

---
+++ GithubPractice/GithubPractice/View/LearnerViews/CueView.swift
@@ -0,0 +1,18 @@
+//
+//  CueView.swift
+//  GithubPractice
+//
+//  Created by 채유성 on 5/26/26.
+//
+
+import SwiftUI
+
+struct CueView: View {
+    var body: some View {
+        Text("나는 유성 채 이다")
+    }
+}
+
+#Preview {
+    CueView()
+}
