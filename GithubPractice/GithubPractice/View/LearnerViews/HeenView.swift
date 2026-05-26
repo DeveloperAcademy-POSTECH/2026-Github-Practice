612225d5423543074a3e2a9da6a8e38f2d87989a
jeewonj <jwjeon.jwj@gmail.com>
2026-05-26 17:14:02 +0900

    HeenView 만들기

---
+++ GithubPractice/GithubPractice/View/LearnerViews/HeenView.swift
@@ -0,0 +1,23 @@
+//
+//  HeenView.swift
+//  GithubPractice
+//
+//  Created by jeegarden on 5/26/26.
+//
+import SwiftUI
+
+struct HeenView: LearnerView {
+    var name: String = "heen"
+    
+    var team: String = "05"
+    
+    var body: some View {
+        
+        Text("안녕하세요")
+    }
+}
+
+#Preview {
+    HeenView()
+}
+
