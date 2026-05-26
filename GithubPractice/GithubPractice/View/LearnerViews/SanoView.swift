10e1281b4482c81da34f597a37b8d83bc811db24
Sano <sion607@naver.com>
2026-05-26 12:28:18 +0900

    고산바보 존바보 샨바보

---
+++ GithubPractice/GithubPractice/View/LearnerViews/SanoView.swift
@@ -0,0 +1,18 @@
+//
+//  SanoView.swift
+//  GithubPractice
+//
+//  Created by 이시온 on 5/26/26.
+//
+
+import SwiftUI
+
+struct SanoView: View {
+    
+    var body: some View {
+        
+        Text("으악!")
+        
+    }
+    
+}
