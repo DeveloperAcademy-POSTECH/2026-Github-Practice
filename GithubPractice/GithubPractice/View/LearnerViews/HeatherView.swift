1451515800d26b1e5e3270ba18df8ae5a61c334e
kkdew3 <kk808@swu.ac.kr>
2026-05-26 17:13:31 +0900

    헤더 뷰를 만들었음

---
+++ GithubPractice/GithubPractice/View/LearnerViews/HeatherView.swift
@@ -0,0 +1,22 @@
+//
+//  HeatherView.swift
+//  GithubPractice
+//
+//  Created by 김민서 on 5/26/26.
+//
+
+import SwiftUI
+
+struct HeatherView: LearnerView {
+    var name: String = "Heather"
+    
+    var team: String = "10"
+    
+    var body: some View {
+        Text("아이고 아이고")
+    }
+}
+
+#Preview {
+    HeatherView()
+}
