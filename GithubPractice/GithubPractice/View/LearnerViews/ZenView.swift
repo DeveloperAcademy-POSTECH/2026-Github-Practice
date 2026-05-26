2a39b0a4aa4ff2e9f9e090592c806fc93c9a2069
Yoonjin Lee <yoonjin9150@gmail.com>
2026-05-26 17:13:50 +0900

    하이 젠
    젠뷰 만들엇음 ㅋ

---
+++ GithubPractice/GithubPractice/View/LearnerViews/ZenView.swift
@@ -0,0 +1,22 @@
+//
+//  ZenView.swift
+//  GithubPractice
+//
+//  Created by 이윤진 on 5/26/26.
+//
+
+import SwiftUI
+
+struct ZenView: LearnerView {
+    var name: String = "Zen"
+    
+    var team: String = "13"
+    
+    var body: some View {
+        Text("안녕하세요 제니입니다")
+    }
+}
+
+#Preview {
+    ZenView()
+}
