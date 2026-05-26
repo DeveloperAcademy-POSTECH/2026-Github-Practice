306a76289d328ca1978ed96f35c428c67e614c08
Minjae Kwon <stewberry326@gmail.com>
2026-05-26 17:13:34 +0900

    Create PurpleView.swift

---
+++ GithubPractice/GithubPractice/View/LearnerViews/PurpleView.swift
@@ -0,0 +1,20 @@
+//
+//  PurpleView.swift
+//  GithubPractice
+//
+//  Created by Minjae Kwon on 5/26/26.
+//
+
+import SwiftUI
+
+struct PurpleView: LearnerView {
+    var name: String = "Purple"
+    var team: String = "VisionCrash"
+    var body: some View {
+        Text("🟪🟪🟪🟪🟪🟪🟪🟪")
+    }
+}
+
+#Preview {
+    PurpleView()
+}
