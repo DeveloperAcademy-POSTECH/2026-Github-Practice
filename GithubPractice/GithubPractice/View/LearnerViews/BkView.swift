b119ed300c28f936e80f61624b15601c3b5be346
BK <music.bk@gmail.com>
2026-05-26 17:13:46 +0900

    bk added

---
+++ GithubPractice/GithubPractice/View/LearnerViews/BkView.swift
@@ -0,0 +1,23 @@
+//
+//  BkView.swift
+//  GithubPractice
+//
+//  Created by BK on 5/26/26.
+//
+
+import SwiftUI
+
+struct BkView: LearnerView {
+    var name: String = "BK"
+    
+    var team: String = "2"
+    
+    var body: some View {
+        Text("Hello, it's BK!")
+    }
+    
+}
+
+#Preview {
+    BkView()
+}
