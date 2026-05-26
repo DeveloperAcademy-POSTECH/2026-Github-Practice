b25548c9b68fcb0f5cf7cdf3cc9f91370165e690
Sunnnny-ssu-ADA <jaeyu26@pos.idserve.net>
2026-05-26 12:28:26 +0900

    Ssu view 추가

---
+++ GithubPractice/GithubPractice/View/LearnerViews/SsuView.swift
@@ -0,0 +1,27 @@
+//
+//  SsuView.swift
+//  GithubPractice
+//
+//  Created by Sunny Side-Up on 5/26/26.
+//
+
+
+import SwiftUI
+
+struct SsuView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Ssu"
+    
+    var team: String = "지미집"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    SsuView()
+}
