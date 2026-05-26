4a5f680a409773dcce35bccf264ddb3108c92ae6
Shin Seoyeon <rucy2428@postech.ac.kr>
2026-05-26 17:13:51 +0900

    수리뷰skrr

---
+++ GithubPractice/GithubPractice/View/LearnerViews/SooriView.swift
@@ -0,0 +1,22 @@
+//
+//  SooriView.swift
+//  GithubPractice
+//
+//  Created by Soori on 5/26/26.
+//
+
+import SwiftUI
+
+struct SooriView: LearnerView {
+    var name: String = "Soori"
+    
+    var team: String = "7"
+    
+    var body: some View {
+        Text("수리수리마수리\n수리가 주는 수리스타를 받아랏􀋃􀋃􀋃")
+    }
+}
+
+#Preview {
+    SooriView()
+}
