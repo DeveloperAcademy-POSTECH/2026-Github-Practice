6239b5d6b86394fe310d47f51ff601397f5c6537
minddoi <minddoi@cau.ac.kr>
2026-05-26 17:13:36 +0900

    제나뷰 추가했어요

---
+++ GithubPractice/GithubPractice/View/LearnerViews/JennaView.swift
@@ -0,0 +1,22 @@
+//
+//  JennaView.swift
+//  GithubPractice
+//
+//  Created by JENNA on 5/26/26.
+//
+
+import SwiftUI
+
+struct JennaView: LearnerView {
+    let name: String = "Jenna"
+    
+    let team: String = "team"
+    
+    var body: some View {
+        Text("안녕녕 제나난")
+    }
+}
+
+#Preview {
+    JennaView()
+}
