313445d19e84aba377f2f6ebd9c32e0c7c097f79
9aeunnn <gaeun_2020@naver.com>
2026-05-26 12:28:31 +0900

    [Feat] 리스트에 Ganni 뷰 추가

--- GithubPractice/GithubPractice/View/LearnerViews/GanniView.swift
+++ GithubPractice/GithubPractice/View/LearnerViews/GanniView.swift
@@ -10,7 +10,7 @@ struct GanniView: LearnerView {
     // TODO: 1. 이름, 팀 수정하고 커밋하기
     var name: String = "Ganni"
     
-    var team: String = "뗔ㅋㅋ"
+    var team: String = "뗔ㅋㅋ팀"
     
     var body: some View {
         Text("저의 이름은 \(name)입니다.")
