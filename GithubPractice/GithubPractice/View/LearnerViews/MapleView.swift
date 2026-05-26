982c492894786a662118f09f6ef1c3f91e02cff9
neve1n <seojin060504@pukyong.ac.kr>
2026-05-26 17:14:07 +0900

    [Feat] {Maple} 뷰 제작

---
+++ GithubPractice/GithubPractice/View/LearnerViews/MapleView.swift
@@ -0,0 +1,22 @@
+//
+//  MapleView.swift
+//  GithubPractice
+//
+//  Created by 최서진 on 5/26/26.
+//
+
+import SwiftUI
+
+struct MapleView: LearnerView {
+    var name: String = "Maple"
+    
+    var team: String = "이어폰줄꼬임"
+    
+    var body: some View {
+        Text("안녕하세요")
+    }
+}
+
+#Preview {
+    MapleView()
+}
