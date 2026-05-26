796b65da65734030b86cd44b956738f6cfbe5b72
Turto <121936896+ErinKYB@users.noreply.github.com>
2026-05-26 12:35:00 +0900

    [Update] ErinView 추가

--- GithubPractice/GithubPractice/View/LearnerViews/ErinView.swift
+++ GithubPractice/GithubPractice/View/LearnerViews/ErinView.swift
@@ -7,9 +7,18 @@
 
 import SwiftUI
 
-struct ErinView: View {
+struct ErinView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Erin"
+    
+    var team: String = "14팀"
+    
     var body: some View {
-        Text("I am Erin. \nAmaze Amaze Amaze!")
+        Text("저의 이름은 Erin입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 도원결의입니다.")
+          .font(.subheadline)
+        
     }
 }
 
