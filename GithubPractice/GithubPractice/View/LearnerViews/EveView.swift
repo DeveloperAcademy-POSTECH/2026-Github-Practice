e40273951d1e5dedad5b94f7f46b6d1642c31bec
Eunbin Kim(Eve) <asi031@naver.com>
2026-05-26 17:13:47 +0900

    이브가 커밋을 하겠노라

---
+++ GithubPractice/GithubPractice/View/LearnerViews/EveView.swift
@@ -0,0 +1,22 @@
+//
+//  EveView.swift
+//  GithubPractice
+//
+//  Created by KIM EUNBIN on 5/26/26.
+//
+
+import SwiftUI
+
+struct EveView: LearnerView {
+    var name: String = "Eve"
+    
+    var team: String = "15"
+    
+    var body: some View {
+        Text("안녕하세요 이브입니당")
+    }
+}
+
+#Preview {
+    EveView()
+}
