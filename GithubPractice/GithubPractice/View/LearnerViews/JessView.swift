0dfe1700f4f02c2768a5306595364486a849ed0a
Jess <jinokbiz@gmail.com>
2026-05-26 12:27:38 +0900

    JessView 추가

---
+++ GithubPractice/GithubPractice/View/LearnerViews/JessView.swift
@@ -0,0 +1,26 @@
+//
+//  JessView.swift
+//  GithubPractice
+//
+//  Created by Jess on 5/26/26.
+//
+
+import SwiftUI
+
+struct JessView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Jess"
+    
+    var team: String = "Jason & Senny"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    JessView()
+}
