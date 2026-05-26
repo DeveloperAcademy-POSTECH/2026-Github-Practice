05f9d54a56f08d98faadd5ed8467f1ec3b354bc7
Karin(이진아) <victoryjina@gmail.com>
2026-05-26 17:13:48 +0900

    commit_LEO

---
+++ GithubPractice/GithubPractice/View/LearnerViews/LEO.swift
@@ -0,0 +1,23 @@
+//
+//  LEO.swift
+//  GithubPractice
+//
+//  Created by Karin on 5/26/26.
+//
+
+import SwiftUI
+
+struct LEO: View {
+    
+    var name: String = "Leeo"
+    
+    var team: String = "13"
+    
+    var body: some View {
+        Text("안녕하세요, 리이오입니다!")
+    }
+}
+
+#Preview {
+    LEO()
+}
