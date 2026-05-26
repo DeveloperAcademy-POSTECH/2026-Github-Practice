a750d1894869019ee89eb73bed0413ef81ede1df
YoungJin Jeong <81943525+luminouxx@users.noreply.github.com>
2026-05-26 09:31:03 +0900

    연습과제 수정

---
+++ GithubPractice/GithubPractice/Model/LearnerViews.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  GithubPractice
+//
+//  Created by 정영진 on 5/22/25.
+//
+
+import Foundation
+
+// TODO: 2. learnerViews에 자신의 View 추가하고 커밋!
+let learnerViews: [any LearnerView] = [
+    LumiView(),
+    JudyView(),
+    FridayView(),
+    ElenaView(),
+    JudyJView(),
+    MaryView()
+]
