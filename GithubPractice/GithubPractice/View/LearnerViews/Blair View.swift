a792001a8922abeea42179638c6477a5adcb4129
Boyoon Kim <lima65640714@gmail.com>
2026-05-26 12:27:51 +0900

    블레어의 첫번째 작업

---
+++ GithubPractice/GithubPractice/View/LearnerViews/Blair View.swift
@@ -0,0 +1,18 @@
+//
+//  Blair View.swift
+//  GithubPractice
+//
+//  Created by Boyoon Kim on 5/26/26.
+//
+
+import SwiftUI
+
+struct Blair_View: View {
+    var body: some View {
+        Text("저는 블레어입니다. 옆에서 유키가 보고있어요.")
+    }
+}
+
+#Preview {
+    Blair_View()
+}
