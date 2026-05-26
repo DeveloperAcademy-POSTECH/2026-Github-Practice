918507910d79b2e8885c3891e8c62a9a62c887c3
hobbypedia <hobbyist916@gmail.com>
2026-05-26 12:28:27 +0900

    BinoView 파일 생성

---
+++ GithubPractice/GithubPractice/View/LearnerViews/BinoView.swift
@@ -0,0 +1,26 @@
+//
+//  BinoView.swift
+//  GithubPractice
+//
+//  Created by BinoKim on 5/26/25.
+//
+
+import SwiftUI
+
+struct BinoView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Bino"
+    
+    var team: String = "러너팀"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    BinoView()
+}
