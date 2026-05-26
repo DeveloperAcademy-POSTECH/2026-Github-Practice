5c240f0c814b8fb5cfc5c55fabe2fe2ca81ca51d
Jiwon Hong <enog.arch@gmail.com>
2026-05-26 17:13:42 +0900

    스페로 뷰 ㅋ

---
+++ GithubPractice/GithubPractice/View/LearnerViews/SperoView.swift
@@ -0,0 +1,22 @@
+//
+//  SperoView.swift
+//  GithubPractice
+//
+//  Created by jiwon hong on 5/26/26.
+//
+
+import SwiftUI
+
+struct SperoView: LearnerView {
+    var name: String = "Spero"
+    
+    var team: String = "team7"
+    
+    var body: some View {
+        Text("집에 가고 싶어요")
+    }
+}
+
+#Preview {
+    SperoView()
+}
