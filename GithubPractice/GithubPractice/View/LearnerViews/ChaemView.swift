eb72b10fbdc03eac931df4aa51527fd2ed1fad37
chami3i <hongcm2002@naver.com>
2026-05-26 12:26:39 +0900

    Update ChaemView.swift

--- GithubPractice/GithubPractice/View/LearnerViews/ChaemView.swift
+++ GithubPractice/GithubPractice/View/LearnerViews/ChaemView.swift
@@ -5,3 +5,21 @@
 //  Created by chaem on 5/26/26.
 //
 
+import SwiftUI
+
+struct ChaemView: LearnerView {
+    var name: String = "Chaem"
+    
+    var team: String = "룰러살몬"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    ChaemView()
+}
