e0f5bc2db96518fff086c3e4e05b3a66ba532723
eunacandy <euncho26@pos.idserve.net>
2026-05-26 17:13:51 +0900

    앤뷰 커밋

---
+++ GithubPractice/GithubPractice/View/LearnerViews/AnneView.swift
@@ -0,0 +1,25 @@
+//
+//  AnneView.swift
+//  GithubPractice
+//
+//  Created by Euna Cho on 5/26/26.
+//
+
+import SwiftUI
+
+struct AnneView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Anne"
+    
+    var team: String = "이어폰줄꼬임"
+    
+    var body: some View {
+        Text("안녕하세요 앤이에요")
+    
+       
+    }
+}
+
+#Preview {
+    AnneView()
+}
