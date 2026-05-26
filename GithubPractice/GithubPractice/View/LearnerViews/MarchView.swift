21b0d6e4aac4f88d01daf7eaa896cb21c818ee75
hyunzimn <hyunzimn@gmail.com>
2026-05-26 17:14:24 +0900

    마치 뷰 만들엇어염 ㅋㅋ

---
+++ GithubPractice/GithubPractice/View/LearnerViews/MarchView.swift
@@ -0,0 +1,22 @@
+//
+//  MarchView.swift
+//  GithubPractice
+//
+//  Created by Jimin Hyun on 5/26/26.
+//
+
+import SwiftUI
+
+struct MarchView: LearnerView {
+    let name: String = "March"
+    
+    let team: String = "Team 6 오도독"
+    
+    var body: some View {
+        Text("안녕하세요 마치예염")
+    }
+}
+
+#Preview {
+    MarchView()
+}
