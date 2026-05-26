ad7d4744e16fbb54426c9eea922b2c940aeb34ba
BIBI <ggg8504@gmail.com>
2026-05-26 17:13:46 +0900

    어렵다유 비비

---
+++ GithubPractice/GithubPractice/View/LearnerViews/BIBIView.swift
@@ -0,0 +1,22 @@
+//
+//  BIBIView.swift
+//  GithubPractice
+//
+//  Created by bibi on 5/26/26.
+//
+
+import SwiftUI
+
+struct BIBIView: LearnerView {
+    var name: String = "BIBI"
+    
+    var team: String = "1"
+    
+    var body: some View {
+        Text("배고파요")
+    }
+}
+
+#Preview {
+    BIBIView()
+}
