8ecbfa63dcac99bd82e81d6801d2be076cad013a
DevKEI-iOS <ifdikei@gmail.com>
2026-05-26 17:13:32 +0900

    안녕하세용 우린 배치깁니다

---
+++ GithubPractice/GithubPractice/View/LearnerViews/KeiView.swift
@@ -0,0 +1,26 @@
+//
+//  KeiView.swift
+//  GithubPractice
+//
+//  Created by 서혜린 on 5/26/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct KeiView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Kei"
+    
+    var team: String = "9팀"
+    
+    var body: some View {
+        Text("안녕하세요 케케케케이이이이")
+            
+
+    }
+}
+
+#Preview {
+    KeiView()
+}
