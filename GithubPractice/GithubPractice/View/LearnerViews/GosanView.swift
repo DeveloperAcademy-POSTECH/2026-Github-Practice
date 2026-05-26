147c05d30807efe8e41e1f6af87c41ea219c3ba8
LEE CHIHOON <dlclgns152615@gmail.com>
2026-05-26 12:32:05 +0900

    [UI] add GosanView
    goood~

---
+++ GithubPractice/GithubPractice/View/LearnerViews/GosanView.swift
@@ -0,0 +1,18 @@
+//
+//  FridayView.swift
+//  GithubPractice
+//
+//  Created by Gosan on 5/26/26.
+//
+
+import SwiftUI
+
+struct GosanView: LearnerView {
+    var name: String = "Gosan"
+    
+    var team: String = "러너팀"
+    
+    var body: some View {
+        Text("Hello, Gosan!")
+    }
+}
