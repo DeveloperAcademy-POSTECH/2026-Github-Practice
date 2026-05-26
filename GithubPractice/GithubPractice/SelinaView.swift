2534efaa099fcb1c33901ea32f69a4f862c229ff
Selina0515 <nahkim26@pos.idserve.net>
2026-05-26 12:27:27 +0900

    이름 적음

---
+++ GithubPractice/GithubPractice/SelinaView.swift
@@ -0,0 +1,18 @@
+//
+//  SelinaView.swift
+//  GithubPractice
+//
+//  Created by 김나현 on 5/26/26.
+//
+
+import SwiftUI
+
+struct SelinaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelinaView()
+}
