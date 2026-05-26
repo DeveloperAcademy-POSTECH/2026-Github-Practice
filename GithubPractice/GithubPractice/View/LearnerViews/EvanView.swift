d0d17b991ff6481bb51f0b0d7c8a6ca257219d46
Evan-Apple <hdjdev@outlook.com>
2026-05-26 12:27:51 +0900

    에반의 첫번째 작업

---
+++ GithubPractice/GithubPractice/View/LearnerViews/EvanView.swift
@@ -0,0 +1,18 @@
+//
+//  EvanView.swift
+//  GithubPractice
+//
+//  Created by user on 5/26/26.
+//
+
+import SwiftUI
+
+struct EvanView: View {
+    var body: some View {
+        Text("나는 에반이다.")
+    }
+}
+
+#Preview {
+    EvanView()
+}
