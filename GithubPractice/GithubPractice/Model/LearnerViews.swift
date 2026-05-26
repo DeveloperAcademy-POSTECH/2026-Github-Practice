088b0282d1a60f917adb8ac417893484b2e42a5b
Choiyunseo <148309729+Happymondayy@users.noreply.github.com>
2026-05-26 12:47:57 +0900

    Revert "#41 kou"

--- GithubPractice/GithubPractice/Model/LearnerViews.swift
+++ GithubPractice/GithubPractice/Model/LearnerViews.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 // TODO: 2. learnerViews에 자신의 View 추가하고 커밋!
 let learnerViews: [any LearnerView] = [
-    KouView(),
     LumiView(),
     JudyView(),
     DarrenView()
@@ -17,5 +16,4 @@ let learnerViews: [any LearnerView] = [
     KayaView(),
     KamchoView(),
     FridayView()
-
 ]
