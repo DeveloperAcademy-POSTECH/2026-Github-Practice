b48e37cce13c76cc10e08b80eb8db53318edafc2
daayye <estherleedy@gmail.com>
2026-05-26 17:13:42 +0900

    다니 뷰 생성

---
+++ GithubPractice/GithubPractice/View/LearnerViews/DaniView.swift
@@ -0,0 +1,23 @@
+//
+//  DaniView.swift
+//  GithubPractice
+//
+//  Created by Daye Lee on 5/26/26.
+//
+
+import SwiftUI
+
+struct DaniView: View {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Dani"
+    
+    var team: String = "14"
+    
+    var body: some View {
+        Text("나는 다니다.")
+    }
+}
+
+#Preview {
+    DaniView()
+}
