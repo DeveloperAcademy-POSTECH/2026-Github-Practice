85ba0991d79806a092790e5fc659a87a973f39fb
한민주 <mju0805@naver.com>
2026-05-26 17:13:46 +0900

    테이뷰 만들엇서용

---
+++ GithubPractice/GithubPractice/View/LearnerViews/TayView.swift
@@ -0,0 +1,25 @@
+//
+//  TayView.swift
+//  GithubPractice
+//
+//  Created by 한민주 on 5/26/26.
+//
+
+import SwiftUI
+
+struct TayView: LearnerView {
+    let name: String = "Tay"
+    
+    let team: String = "team9"
+    
+    var body: some View {
+        Text("안녕하세요. 테이입니다.")
+    
+        
+    }
+    
+}
+
+#Preview {
+    TayView()
+}
