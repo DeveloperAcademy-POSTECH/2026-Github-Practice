aac867a7d03c780ad1e908fe3f404b5ad1ba9cca
garasani <garasani10@gmail.com>
2026-05-26 12:27:51 +0900

    살룻의 첫번째 작업

---
+++ GithubPractice/GithubPractice/View/LearnerViews/SaludView.swift
@@ -0,0 +1,19 @@
+//
+//  SaludView.swift
+//  GithubPractice
+//
+//  Created by KDHA on 5/26/26.
+//
+
+import SwiftUI
+
+struct SaludView: View {
+    var body: some View {
+        Text("나는 살룻이다.")
+
+    }
+}
+
+#Preview {
+    SaludView()
+}
