7aa0bc7c630fc7b285642e03e2f86233ca3ac00b
YoungJin Jeong <81943525+luminouxx@users.noreply.github.com>
2026-05-26 12:27:43 +0900

    프라이데이의 첫번쨰 작업

---
+++ GithubPractice/GithubPractice/View/LearnerViews/FridayView.swift
@@ -0,0 +1,18 @@
+//
+//  FridayView.swift
+//  GithubPractice
+//
+//  Created by luminoux on 5/26/26.
+//
+
+import SwiftUI
+
+struct FridayView: View {
+    var body: some View {
+        Text("나는 프라이데이다.")
+    }
+}
+
+#Preview {
+    FridayView()
+}
