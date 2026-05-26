c242f59fc9fc19d91bd50f99985358a2aaadb727
hana5111 <1234123ohn@gmail.com>
2026-05-26 12:27:52 +0900

    이치의 첫번째 작업

---
+++ GithubPractice/GithubPractice/View/LearnerViews/IchiView.swift
@@ -0,0 +1,18 @@
+//
+//  IchiView.swift
+//  
+//
+//  Created by 오하나 on 5/26/26.
+//
+
+import SwiftUI
+
+struct IchiView: View {
+    var body: some View {
+        Text("나는 이치다.")
+    }
+}
+
+#Preview {
+    IchiView()
+}
