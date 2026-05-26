cd5734972cf9a064f46eed8849dc5320e675891e
kigogo99 <gogoruru9956@gmail.com>
2026-05-26 17:15:08 +0900

    [Feat] {유메} 뷰 제작

---
+++ GithubPractice/GithubPractice/View/LearnerViews/YOUMEIView.swift
@@ -0,0 +1,18 @@
+//
+//  YOUMEIView.swift
+//  GithubPractice
+//
+//  Created by ki on 5/26/26.
+//
+
+import SwiftUI
+
+struct YOUMEIView: LearnerView{
+    var name: String = "YOUMEI"
+    
+    var team: String = "YOUMEI"
+    
+    var body: some View{
+        Text("안녕하시소")
+    }
+}
