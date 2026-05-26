7297304af8c05d6ee6541abc73c1a80e9a09fa14
Jaewon Kim <83jason@naver.com>
2026-05-26 17:13:40 +0900

    제이스View 만듦 ㅋ

---
+++ GithubPractice/GithubPractice/View/LearnerViews/JaceView.swift
@@ -0,0 +1,22 @@
+//
+//  JaceView.swift
+//  GithubPractice
+//
+//  Created by Jaewon Kim on 5/26/26.
+//
+
+import SwiftUI
+
+struct JaceView: LearnerView {
+    var name: String = "Jace"
+    
+    var team: String = "14"
+    
+    var body: some View {
+        Text("hi everyone:)")
+    }
+}
+
+#Preview {
+    JaceView()
+}
