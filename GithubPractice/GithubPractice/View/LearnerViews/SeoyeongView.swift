5f4d0b0292c05d235be7e5e8c8472fa78ae2dbbe
seoyss <seoyeongshin01@gmail.com>
2026-05-26 12:31:06 +0900

    SeoyeongView 만들었슈

---
+++ GithubPractice/GithubPractice/View/LearnerViews/SeoyeongView.swift
@@ -0,0 +1,25 @@
+//
+//  SeoyeongView.swift
+//  GithubPractice
+//
+//  Created by Seoyeong Shin on 5/26/26.
+//
+
+import SwiftUI
+
+struct SeoyeongView: LearnerView {
+    var name: String = "Seoyeong"
+    
+    var team: String = "오전팀"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    SeoyeongView()
+}
