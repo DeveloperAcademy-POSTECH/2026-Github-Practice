2b1569fae53978829e503ea504c50ad68eb8fe67
Yetii Cho <yerich0119@gmail.com>
2026-05-26 12:27:49 +0900

    예티의 첫번째 작업

---
+++ GithubPractice/GithubPractice/View/LearnerViews/YetiiView.swift
@@ -0,0 +1,26 @@
+//
+//  YetiiView.swift
+//  GithubPractice
+//
+//  Created by Yetii Cho on 5/26/26.
+//
+
+import SwiftUI
+
+struct YetiiView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Yetii"
+    
+    var team: String = "비전프린세스"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    YetiiView()
+}
