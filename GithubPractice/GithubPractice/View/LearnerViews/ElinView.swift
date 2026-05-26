bd0b7606aed9762a7402bc49f4d8d532d90f54e0
Kim Dohyeon <elena.kim.0915@gmail.com>
2026-05-26 12:33:24 +0900

    [Feat] 리스트에 {Elin} 뷰 추가

--- GithubPractice/GithubPractice/View/LearnerViews/ElinView.swift
+++ GithubPractice/GithubPractice/View/LearnerViews/ElinView.swift
@@ -1,16 +1,23 @@
 //
-//  ElinView.swift
+//  LumiView.swift
 //  GithubPractice
 //
-//  Created by 김도현 on 5/26/26.
+//  Created by 정영진 on 5/22/25.
 //
 
 import SwiftUI
 
-struct ElinView: View {
+struct ElinView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Elin"
+    
+    var team: String = "조용한야망"
+    
     var body: some View {
-        Text("이름: Elin/김도현 팀: 조용한 야망")
-        
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
     }
 }
 
